c66178a3eb73d724783350ba5e61b517d558eed4
Gen11-Enterpeises <gen11enterpriseofficial@gmail.com>
2026-07-11 12:18:40 +0530

    Update gallery.js

--- assets/gallery.js
+++ assets/gallery.js
@@ -11,9 +11,14 @@ async function loadFiles() {
   }
 
   const container = document.getElementById('file-list')
-  container.innerHTML = data.map(file =>
-    `<div><h3>${file.title}</h3><img src="${file.file_url}" width="200"></div>`
-  ).join('')
+  container.innerHTML = data.map(file => `
+    <figure class="gallery-item" data-cat="campus" data-reveal>
+      <button data-lightbox="${file.file_url}" data-caption="${file.title}" style="all:unset;cursor:pointer;display:block;width:100%;">
+        <img src="${file.file_url}" alt="${file.title}" loading="lazy">
+        <figcaption><span class="tag">${file.title}</span></figcaption>
+      </button>
+    </figure>
+  `).join('')
 }
 
 loadFiles()
